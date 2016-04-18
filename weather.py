e2e48843b9287927ad77e02bd9e41f7f323b9f0d
Javier <javicho21@gmail.com>
2016-04-18 10:34:06 -0300

    changed to line format

--- weather.py
+++ weather.py
@@ -27,22 +27,34 @@ precip_today_in = (mylist2[0])
 mylist3 = time_stamp_jason.split("'")
 time_stamp = int(mylist3[0])*1000
 
-msg ='{\nmetric: "%s",\ndatapoints: [\n{\ntags: {"city":"Manhattan","state":"NY","latitude": "40.750.13351","longitude":"-73.99700928","neighborhood":"Chelsea","country":"US","elevation":"110","station_id":"KNYNEWYO395","rpi.datatype":"METRIC","sensor.unit":"F"},\nvalues: {"%s":"%s"}\n}]\n}' % ("Temperature",time_stamp,temp_f_jason)
+
+#msg ='{\nmetric: "%s",\ndatapoints: [\n{\ntags: {"city":"Manhattan","state":"NY","latitude": "40.750.13351","longitude":"-73.99700928","neighborhood":"Chelsea","country":"US","elevation":"110","station_id":"KNYNEWYO395","rpi.datatype":"METRIC","sensor.unit":"F"},\nvalues: {"%s":"%s"}\n}]\n}' % ("Temperature",time_stamp,temp_f_jason)
+
+msg ='%s,city:Manhattan,state:NY,latitude:40.750.13351,longitude:-73.99700928,neighborhood:Chelsea,country:US,elevation:110,station_id:KNYNEWYO395,rpi.datatype:METRIC,sensor.unit:F,sensor.name=%s value=%s %s\n' % ("Temperature","weatherTemperature",temp_f_jason,time_stamp)
 print msg
 client.publish("javier/board1",msg)
 time.sleep(0.1)
 
-msg ='{\nmetric: "%s",\ndatapoints: [\n{\ntags: {"city":"Manhattan","state":"NY","latitude": "40.750.13351","longitude":"-73.99700928","neighborhood":"Chelsea","country":"US","elevation":"110","station_id":"KNYNEWYO395","rpi.datatype":"METRIC","sensor.unit":"%s"},\nvalues: {"%s":"%s"}\n}]\n}' % ("relative_humidity","%",time_stamp,relative_humidity)
+
+#msg ='{\nmetric: "%s",\ndatapoints: [\n{\ntags: {"city":"Manhattan","state":"NY","latitude": "40.750.13351","longitude":"-73.99700928","neighborhood":"Chelsea","country":"US","elevation":"110","station_id":"KNYNEWYO395","rpi.datatype":"METRIC","sensor.unit":"%s"},\nvalues: {"%s":"%s"}\n}]\n}' % ("relative_humidity","%",time_stamp,relative_humidity)
+
+msg ='%s,city:Manhattan,state:NY,latitude:40.750.13351,longitude:-73.99700928,neighborhood:Chelsea,country:US,elevation:110,station_id:KNYNEWYO395,rpi.datatype:METRIC,sensor.unit:%s,sensor.name=%s value=%s %s\n' % ("relative_humidity","%","weatherHumidity",relative_humidity,time_stamp)
 print msg
 client.publish("javier/board1",msg)
 time.sleep(0.1)
 
-msg ='{\nmetric: "%s",\ndatapoints: [\n{\ntags: {"city":"Manhattan","state":"NY","latitude": "40.750.13351","longitude":"-73.99700928","neighborhood":"Chelsea","country":"US","elevation":"110","station_id":"KNYNEWYO395","rpi.datatype":"METRIC","sensor.unit":"mph"},\nvalues: {"%s":"%s"}\n}]\n}' % ("Wind",time_stamp,wind_mph_jason)
+
+#msg ='{\nmetric: "%s",\ndatapoints: [\n{\ntags: {"city":"Manhattan","state":"NY","latitude": "40.750.13351","longitude":"-73.99700928","neighborhood":"Chelsea","country":"US","elevation":"110","station_id":"KNYNEWYO395","rpi.datatype":"METRIC","sensor.unit":"mph"},\nvalues: {"%s":"%s"}\n}]\n}' % ("Wind",time_stamp,wind_mph_jason)
+
+msg ='%s,city:Manhattan,state:NY,latitude:40.750.13351,longitude:-73.99700928,neighborhood:Chelsea,country:US,elevation:110,station_id:KNYNEWYO395,rpi.datatype:METRIC,sensor.unit:mph,sensor.name=%s value=%s %s\n' % ("Wind","weatherWind",wind_mph_jason,time_stamp)
 print msg
 client.publish("javier/board1",msg)
 time.sleep(0.1)
 
-msg ='{\nmetric: "%s",\ndatapoints: [\n{\ntags: {"city":"Manhattan","state":"NY","latitude": "40.750.13351","longitude":"-73.99700928","neighborhood":"Chelsea","country":"US","elevation":"110","station_id":"KNYNEWYO395","rpi.datatype":"METRIC","sensor.unit":"in"},\nvalues: {"%s":"%s"}\n}]\n}' % ("Precipitation",time_stamp,precip_today_in)
+
+#msg ='{\nmetric: "%s",\ndatapoints: [\n{\ntags: {"city":"Manhattan","state":"NY","latitude": "40.750.13351","longitude":"-73.99700928","neighborhood":"Chelsea","country":"US","elevation":"110","station_id":"KNYNEWYO395","rpi.datatype":"METRIC","sensor.unit":"in"},\nvalues: {"%s":"%s"}\n}]\n}' % ("Precipitation",time_stamp,precip_today_in)
+
+msg ='%s,city:Manhattan,state:NY,latitude:40.750.13351,longitude:-73.99700928,neighborhood:Chelsea,country:US,elevation:110,station_id:KNYNEWYO395,rpi.datatype:METRIC,sensor.unit:in,sensor.name=%s value=%s %s\n' % ("Precipitation","weatherPrecipitation",precip_today_in,time_stamp)
 print msg
 client.publish("javier/board1",msg)
 
